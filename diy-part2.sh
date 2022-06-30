729e29b37fdd254ff120b7eb961f29c1bbc6e914
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2022-06-30 16:29:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,12 +12,12 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
-smartdns
+#smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git
 mv openwrt-smartdns/ package/smartdns 
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git 
 mv luci-app-smartdns/ package/
-vssr
+#vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git 
 git clone https://github.com/jerrykuku/luci-app-vssr.git 
 mv luci-app-vssr/ package/
