7bdfdf02b8e8f0550f061cd9de70816ff47a8a69
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2023-12-21 08:48:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
-#smartdns
-#git clone https://github.com/pymumu/openwrt-smartdns.git
-#mv openwrt-smartdns/ package/smartdns 
-#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git 
-#mv luci-app-smartdns/ package/
-#vssr
-#git clone https://github.com/jerrykuku/lua-maxminddb.git 
-#git clone https://github.com/jerrykuku/luci-app-vssr.git 
-#mv lua-maxminddb/ package/
-# Alist
-#git clone https://github.com/sbwml/openwrt-alist --depth=1
-#mv openwrt-alist/ package/alist
+sed -i 's/192.168.1.1/192.168.168.2/g' package/base-files/files/bin/config_generate
+
