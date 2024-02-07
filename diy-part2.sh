fd52a9c659188f55f3a182680118fc18d31e141c
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2024-02-07 08:35:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,3 @@
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.168.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.3/g' package/base-files/files/bin/config_generate
 
