9c06465db5680d83b0d4b36286ec87e6fcf172f9
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2024-02-28 21:50:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,10 @@
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.3/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
 # alist
-rm -rf /feeds/kenzo/alist
-rm -rf /feeds/kenzo/luci-app-alist
-git clone https://github.com/sbwml/luci-app-alist package/alist
+#rm -rf /feeds/kenzo/alist
+#rm -rf /feeds/kenzo/luci-app-alist
+#git clone https://github.com/sbwml/luci-app-alist package/alist
 # v2raya
-rm -rf /feeds/kenzo/v2raya
-rm -rf /feeds/kenzo/luci-app-v2raya
-git clone https://github.com/v2rayA/v2raya-openwrt.git package/v2raya
+#rm -rf /feeds/kenzo/v2raya
+#rm -rf /feeds/kenzo/luci-app-v2raya
+#git clone https://github.com/v2rayA/v2raya-openwrt.git package/v2raya
