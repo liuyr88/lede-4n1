5fd20a2fbddd5e110e50af65ae44adcb2de0e6ea
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2024-02-28 19:27:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,5 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.3/g' package/base-files/files/bin/config_generate
-
+# alist
+rm -rf /feeds/kenzo/alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
