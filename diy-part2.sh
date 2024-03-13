dd41dc5dbde556ace7df73abdfc1997b9a6b8b8d
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2024-03-13 14:40:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,8 @@ sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_gene
 #rm -rf /feeds/kenzo/v2raya
 #rm -rf /feeds/kenzo/luci-app-v2raya
 #git clone https://github.com/v2rayA/v2raya-openwrt.git package/v2raya
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,smartdns}
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+#rm -rf feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/packages/net/{alist,adguardhome,smartdns}
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
